e33a3abbbb12107b651af3000832e9649f60fd8c
Michael Wang <mzw4@cornell.edu>
2013-04-30 23:45:18 -0400

    flask-pymongo4

--- exampleapp.py
+++ exampleapp.py
@@ -119,8 +119,7 @@ app.config.from_object('conf.Config')
 
 app.config['MONGO_URI'] = "mongodb://heroku_app15232410:6gcfdj39cetjlabuvfv3vpove1@ds061777.mongolab.com:61777/heroku_app15232410"
 mongo = PyMongo(app)
-db = mongo.db
-db.pokemon.insert({name: "Pika"})
+mongo.db.pokemon.insert({name: "Pika"})
 
 def get_home():
     return 'https://' + request.host + '/'
