260ef39f0e2f3328d68801c5c4cb6b8487a0d157
Michael Wang <mzw4@cornell.edu>
2013-05-02 23:33:03 -0400

    game view test

--- reversiapp.py
+++ reversiapp.py
@@ -227,7 +227,7 @@ def home():
             db.users.remove()
             current_user = db.User()
             current_user['_id'] = me['id']
-            current_user.name = me['name']
+            current_user['name'] = me['name']
             current_user['current_games'] = []
             current_user.save()
 
@@ -336,15 +336,14 @@ def play_game():
 
         url = request.url
 
-        current_user = db.users.find_one({'_id': me['id']})
-        if not current_user:
-            db.users.remove()
-            current_user = db.User()
-            current_user['_id'] = me['id']
-            current_user.name = me['name']
-            current_user['current_games'] = []
-            current_user.save()
-            
+        # current_user = db.users.find_one({'_id': me['id']})
+        # if not current_user:
+        #     db.users.remove()
+        current_user = db.User()
+        current_user['_id'] = me['id']
+        current_user['name'] = me['name']
+        current_user.save()
+
         opponent_dummy = db.User()
         opponent_dummy['name'] = 'Poopface'
         opponent_dummy.save()
