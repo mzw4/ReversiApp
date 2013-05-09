8ad070e6cd933fb6bd45e5b57790ba19f9fc2fd2
Michael Wang <mzw4@cornell.edu>
2013-05-09 18:15:00 -0400

    like button

--- reversiapp.py
+++ reversiapp.py
@@ -198,12 +198,8 @@ def home():
 
         me = fb_call('me', args={'access_token': access_token})
         fb_app = fb_call(FB_APP_ID, args={'access_token': access_token})
-        # likes = fb_call('me/likes',
-        #                 args={'access_token': access_token, 'limit': 4})
         # friends = fb_call('me/friends',
         #                   args={'access_token': access_token, 'limit': 4})
-        # photos = fb_call('me/photos',
-        #                  args={'access_token': access_token, 'limit': 16})
 
         redir = get_home() + 'close/'
         POST_TO_WALL = ("https://www.facebook.com/dialog/feed?redirect_uri=%s&"
